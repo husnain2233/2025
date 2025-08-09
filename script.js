79036308bfec852d682046ca194c78f4fddbd019
Arabian Falcom metal kitchen LLC <kfueit1122@gmail.com>
2025-08-09 19:29:50 +0400

    java- 1st change

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ const navLinks = document.getElementById("nav-links");
 hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("active");
 });
+const closeButton = document.getElementById("close-button");
+closeButton.addEventListener("click", () => {
+  navLinks.classList.remove("active");
+});
+const navLinksItems = document.querySelectorAll("#nav-links a");
+navLinksItems.forEach(item => {
+  item.addEventListener("click", () => {
+    navLinks.classList.remove("active");
+  });
+});
+const scrollToTopButton = document.getElementById("scroll-to-top");
